34943a873de11ddee507e9cd8df30a8830330065
GeRe87 <89924726+GeRe87@users.noreply.github.com>
2025-06-06 18:34:39 +0200

    Refactor code structure for improved readability and maintainability

--- resources/js/utils/init_main.js
+++ resources/js/utils/init_main.js
@@ -3,11 +3,12 @@ Reveal.initialize({
   // be preserved when the presentation is scaled to fit different
   // resolutions. Can be specified using percentage units.
   width: 960,
-  height: 500,
+  height: 540,
 
   // Factor of the display size that should remain empty around
   // the content
-  margin: 0.2,
+  center: true, // vertical centering
+  margin: 0.1,
 
   // Bounds for smallest/largest possible scale to apply to content
   minScale: 0.2,
@@ -17,5 +18,15 @@ Reveal.initialize({
   parallaxBackgroundHorizontal: 200,
   hash: true,
   touch: true,
-  plugins: [RevealMarkdown, RevealHighlight, RevealNotes, RevealMath.KaTeX],
+
+  // mermaid initialize config
+    mermaid: {
+      flowchart: {
+        curve: 'linear',
+      },
+      inlineStyles: false,  // <-- kein CSS in jeden Container pumpen
+      theme: 'dark',
+      startOnLoad: false
+    },
+  plugins: [RevealMarkdown, RevealHighlight, RevealNotes, RevealMath.KaTeX, RevealMermaid],
 });
